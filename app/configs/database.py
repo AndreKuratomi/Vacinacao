420f26fdb5175efb0ea49f6dfeae1af26962880d
André Kuratomi <andrekuratomi@gmail.com>
2021-11-26 19:18:01 -0300

    refactor: cleancode in all scripts

--- app/configs/database.py
+++ app/configs/database.py
@@ -7,8 +7,3 @@ db = SQLAlchemy()
 def init_app(app: Flask):
     db.init_app(app)
     app.db = db
-
-    # Importe as suas models de uma maneira que faça sentido. Por exemplo: Em uma relação N:N, importe as
-    # tabelas que são referenciadas na relação N:N e, após isso, importe a tabela pivô. Se você tentar importar
-    # a tabela pivô primeiro, a mesma não terá suas dependências satisfeitas para formar a relação N:N, gerando
-    # um erro
